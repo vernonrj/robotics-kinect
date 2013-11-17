88e65340810fe226ee87a7653d54503061e969ee
vernonrj <vernonrjones@gmail.com>
2013-11-17 15:27:29 -0800

    Changed log display duration to 1 second from 3

--- src/nxt/bluetooth.c
+++ src/nxt/bluetooth.c
@@ -29,7 +29,7 @@ void LogMsg(const string& msg)
     eraseDisplay();
     nxtDisplayCenteredTextLine(3, "LOG");
     nxtDisplayCenteredTextLine(4, msg);
-    wait1Msec(3000);
+    wait1Msec(1000);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
